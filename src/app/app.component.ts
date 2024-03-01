3cc56975497a2f25b88ea978803f4019629a25ff
38061027 <joseaugustogomessantos12@gmail.com>
2024-03-01 19:11:54 -0300

    criando estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'weather-app-2';
+  cityName: string = '';
+  imgCloud : string = ''
+  data:any
+
+  constructor(private service: DataService){}
+
+
+  ngOnInit(): void {
+    console.log(this.imgCloud)
+  }
+
+    getWeather(city:string){
+      this.service.getWeather(city).subscribe(res => {
+        this.data = res
+        console.log(res)
+        const apiCountryURL = `http://openweathermap.org/img/wn/${res.weather[0].icon}.png`;
+       this.imgCloud+=apiCountryURL
+        console.log(apiCountryURL)
+     
+      })
+
+    }
+
+  
+
+
 }
+
+
+
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  apiKey: string = 'ae671dc878c69aa8c3eec4ae088294b6'
+  apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid='
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city:string):Observable<any>{
+ 
+    return this.http.get<any>(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}&lang=pt_br`)
+  }
+
+}
