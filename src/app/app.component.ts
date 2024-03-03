8c919f098b236a9786ca3375150d372eab1bd326
38061027 <joseaugustogomessantos12@gmail.com>
2024-03-03 16:57:33 -0300

    estilizando e modificando dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './services/data.service';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'weather-app-2';
   cityName: string = '';
-  imgCloud : string = ''
-  data:any
+  data: any
 
-  constructor(private service: DataService){}
+  constructor(private service: DataService) { }
 
 
   ngOnInit(): void {
-    console.log(this.imgCloud)
+    this.getWeather('maringa')
   }
 
-    getWeather(city:string){
-      this.service.getWeather(city).subscribe(res => {
-        this.data = res
-        console.log(res)
-        const apiCountryURL = `http://openweathermap.org/img/wn/${res.weather[0].icon}.png`;
-       this.imgCloud+=apiCountryURL
-        console.log(apiCountryURL)
-     
+  getWeather(city: string) {
+    this.service.getWeather(city).pipe(
+      map(el => {
+        el.main.temp = el.main.temp.toFixed(0) + ' ºC';
+        el.main.temp_min = el.main.temp_min.toFixed(0) + ' ºC';
+        el.main.temp_max = el.main.temp_max.toFixed(0) + ' ºC';
+        el.weather[0].iconURL = `http://openweathermap.org/img/wn/${el.weather[0].icon}.png`; 
+        return el;
       })
-
-    }
-
-  
-
+    ).subscribe(res => {
+      this.data = res;
+      console.log(res);
+    });
+  }
 
 }
 
